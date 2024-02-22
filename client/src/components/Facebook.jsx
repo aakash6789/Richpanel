54f2974789f76c96d7de2db2b83eb6a96030e834
Aakash <aakash2506c@gmail.com>
2024-02-22 08:48:35 +0530

    ok

--- client/src/components/Facebook.jsx
+++ client/src/components/Facebook.jsx
@@ -3,56 +3,64 @@ import React from 'react';
 import { useEffect } from 'react';
 
 const Facebook = () => {
-    // useEffect(() => {
-    //     // Load the Facebook SDK asynchronously
-    //     function loadFacebookSDK() {
-    //       window.fbAsyncInit = function() {
-    //         FB.init({
-    //           appId: '1468165183808821',
-    //           autoLogAppEvents: true,
-    //           xfbml: true,
-    //           version: 'v19.0'
-    //         });
+    useEffect(() => {
+        // Load the Facebook SDK asynchronously
+        function loadFacebookSDK() {
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId: '1468165183808821',
+              autoLogAppEvents: true,
+              xfbml: true,
+              version: 'v19.0'
+            });
     
-    //         FB.getLoginStatus(function(response) {
-    //           if (response.status === 'connected') {
-    //             console.log('Logged in.');
-    //             // User is logged in, fetch user's information
-    //             FB.api('/me', { fields: 'name, email' }, function(response) {
-    //               console.log('Welcome! Fetching your information...');
-    //               console.log('Good to see you, ' + response.name + '. I see your email address is ' + response.email);
-    //             });
-    //           } else {
-    //             console.log('Not logged in.');
-    //             // User is not logged in, open login dialog
-    //             FB.login(function(response) {
-    //               if (response.authResponse) {
-    //                 console.log('Logged in.');
-    //               } else {
-    //                 console.log('User cancelled login or did not fully authorize.');
-    //               }
-    //             });
-    //           }
-    //         });
-    //       };
+            FB.getLoginStatus(function(response) {
+              if (response.status === 'connected') {
+                console.log('Logged in.');
+                // User is logged in, fetch user's information
+                FB.api('/me', { fields: 'name, email' }, function(response) {
+                  console.log('Welcome! Fetching your information...');
+                  console.log('Good to see you, ' + response.name + '. I see your email address is ' + response.email);
+                });
+              } else {
+                console.log('Not logged in.');
+                // User is not logged in, open login dialog
+                FB.login(function(response) {
+                  if (response.authResponse) {
+                    console.log('Logged in.');
+                  } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                  }
+                });
+              }
+            });
+          };
     
-    //       // Load the SDK asynchronously
-    //       (function(d, s, id) {
-    //         var js, fjs = d.getElementsByTagName(s)[0];
-    //         if (d.getElementById(id)) return;
-    //         js = d.createElement(s); js.id = id;
-    //         js.src = "https://connect.facebook.net/en_US/sdk.js";
-    //         fjs.parentNode.insertBefore(js, fjs);
-    //       }(document, 'script', 'facebook-jssdk'));
-    //     }
+          // Load the SDK asynchronously
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        }
     
-    //     loadFacebookSDK();
-    //   }, []);
+        loadFacebookSDK();
+      }, []);
   return (
     <div>
-      <div id="fb-root"></div>
-<script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v19.0&appId=1468165183808821" nonce="itNYlqNw"></script>
-<div className="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false">Login with facebook</div>
+      <div id="fb-root">
+      <div className="fb-login-button" 
+           data-width="" 
+           data-size="" 
+           data-button-type="" 
+           data-layout="" 
+           data-auto-logout-link="false" 
+           data-use-continue-as="false">
+            Login
+      </div>
+    </div>
     </div>
   )
 }
